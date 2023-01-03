4e3dcecbb3d7b60ec60312fbdc4e6ad63d9222fa
John Ballesteros <johnshift.dev@gmail.com>
2023-01-03 10:23:59 +0800

    chore: add eslint, prettier

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,18 +1,15 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to web!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
-}
+const CustomApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>Lihim App</title>
+    </Head>
+    <main>
+      <Component {...pageProps} />
+    </main>
+  </>
+);
 
 export default CustomApp;
