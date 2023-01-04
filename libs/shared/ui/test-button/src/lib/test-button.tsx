73797c06c6369693ce6462fd0a93098909a46767
John Ballesteros <johnshift.dev@gmail.com>
2023-01-04 20:28:28 +0800

    test(shared-ui-test-button): reuse storybook for unit test

--- libs/shared/ui/test-button/src/lib/test-button.tsx
+++ libs/shared/ui/test-button/src/lib/test-button.tsx
@@ -1,13 +1,12 @@
-import { FC, MouseEventHandler, useReducer } from 'react';
+import { FC, useReducer } from 'react';
 
 import { Button } from '@mantine/core';
 
 type Props = {
   text: string;
-  onClick?: MouseEventHandler;
 };
 
-export const TestButton: FC<Props> = ({ text, onClick }) => {
+export const TestButton: FC<Props> = ({ text }) => {
   const [clicked, toggle] = useReducer((prev) => !prev, false);
 
   return (
