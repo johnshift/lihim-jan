0cded67714a3e5fad8b5b5f712005ae65f11390c
John Ballesteros <johnshift.dev@gmail.com>
2023-01-10 17:11:19 +0800

    test(auth-ui): add signup-form stories + unit tests

---
+++ apps/web/stories/auth/ui/signup-form.stories.tsx
@@ -0,0 +1,125 @@
+/* eslint-disable testing-library/no-node-access */
+import { useForm } from 'react-hook-form';
+
+import { expect } from '@storybook/jest';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+
+import {
+  authAria,
+  authInputProps,
+  authTestId,
+  authTexts,
+} from '@lihim/auth/core';
+import { SignupForm } from '@lihim/auth/ui';
+import { checkInputDefaults } from '@lihim/shared/testutils/storybook';
+
+export default {
+  component: SignupForm,
+  parameters: {
+    darkMode: {
+      current: 'dark', // Enforce dark theme on initial load
+    },
+  },
+} as ComponentMeta<typeof SignupForm>;
+
+const Template: ComponentStory<typeof SignupForm> = (args) => {
+  const { control } = useForm();
+
+  return (
+    <SignupForm
+      isLoading={false}
+      errors={{
+        firstname: false,
+        lastname: false,
+        username: false,
+        email: false,
+        password: false,
+      }}
+      showLogin={() => null}
+      control={control}
+      onSubmit={() => null}
+      {...args}
+    />
+  );
+};
+
+export const Default = Template.bind({});
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  // Locate elements
+  const firstnameInput = canvas.getByPlaceholderText(
+    authInputProps.firstname.placeholder,
+  );
+  const lastnameInput = canvas.getByPlaceholderText(
+    authInputProps.lastname.placeholder,
+  );
+  const usernameInput = canvas.getByPlaceholderText(
+    authInputProps.username.placeholder,
+  );
+  const emailInput = canvas.getByPlaceholderText(
+    authInputProps.email.placeholder,
+  );
+  const passwordInput = canvas.getByPlaceholderText(
+    authInputProps.password.placeholder,
+  );
+  const submitBtn = canvas.getByRole('button', { name: authAria.submitSignup });
+  const loginLink = canvas.getByTestId(authTestId.signupFooterLink);
+
+  // Assertions
+  checkInputDefaults(firstnameInput, authInputProps.firstname);
+  checkInputDefaults(lastnameInput, authInputProps.lastname);
+  checkInputDefaults(usernameInput, authInputProps.username);
+  checkInputDefaults(emailInput, authInputProps.email);
+  checkInputDefaults(passwordInput, {
+    ...authInputProps.password,
+    type: 'password',
+  });
+  expect(submitBtn).toBeVisible();
+  expect(loginLink).toBeVisible();
+  expect(canvas.getByText(authTexts.signupFooter)).toBeVisible();
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  isLoading: true,
+};
+Loading.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  expect(canvas.getByTestId(authTestId.signupLoadingOverlay)).toBeVisible();
+};
+
+export const Errors = Template.bind({});
+Errors.args = {
+  errors: {
+    firstname: true,
+    lastname: true,
+    username: true,
+    email: true,
+    password: true,
+  },
+};
+Errors.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const firstnameInput = canvas.getByPlaceholderText(
+    authInputProps.firstname.placeholder,
+  );
+  const lastnameInput = canvas.getByPlaceholderText(
+    authInputProps.lastname.placeholder,
+  );
+  const usernameInput = canvas.getByPlaceholderText(
+    authInputProps.username.placeholder,
+  );
+  const emailInput = canvas.getByPlaceholderText(
+    authInputProps.email.placeholder,
+  );
+  const passwordInput = canvas.getByPlaceholderText(
+    authInputProps.password.placeholder,
+  );
+  expect(firstnameInput).toHaveAttribute('aria-invalid', 'true');
+  expect(lastnameInput).toHaveAttribute('aria-invalid', 'true');
+  expect(usernameInput).toHaveAttribute('aria-invalid', 'true');
+  expect(emailInput).toHaveAttribute('aria-invalid', 'true');
+  expect(passwordInput.parentElement).toHaveAttribute('aria-invalid', 'true');
+};
