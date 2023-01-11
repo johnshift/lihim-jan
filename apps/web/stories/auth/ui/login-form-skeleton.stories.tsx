e3cd468482d83e637ba2c3a2a61cbea8e93c8bee
John Ballesteros <johnshift.dev@gmail.com>
2023-01-11 13:27:35 +0800

    test(auth-ui): add login-form-skeleton story + unit test

---
+++ apps/web/stories/auth/ui/login-form-skeleton.stories.tsx
@@ -0,0 +1,43 @@
+import { useForm } from 'react-hook-form';
+
+import { Group } from '@mantine/core';
+
+import { expect } from '@storybook/jest';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+
+import { TESTID_LOGIN_SKELETON } from '@lihim/auth/core';
+import { LoginFormSkeleton } from '@lihim/auth/ui';
+import { LoginForm } from '@lihim/auth/ui';
+
+export default {
+  component: LoginFormSkeleton,
+  parameters: {
+    darkMode: {
+      current: 'dark', // Enforce dark theme on initial load
+    },
+  },
+} as ComponentMeta<typeof LoginFormSkeleton>;
+
+const Template: ComponentStory<typeof LoginFormSkeleton> = () => {
+  const { control } = useForm();
+
+  return (
+    <Group>
+      <LoginFormSkeleton />
+      <LoginForm
+        isLoading={false}
+        hasError={false}
+        showSignup={() => null}
+        control={control}
+        onSubmit={() => null}
+      />
+    </Group>
+  );
+};
+
+export const Default = Template.bind({});
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  expect(canvas.getByTestId(TESTID_LOGIN_SKELETON)).toBeVisible();
+};
