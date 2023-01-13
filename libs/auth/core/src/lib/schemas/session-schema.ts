254d3a11441f63d25096d941cab9cae089a79502
John Ballesteros <johnshift.dev@gmail.com>
2023-01-13 09:08:06 +0800

    refactor(auth-core): declare anon/logged-in session schemas

--- libs/auth/core/src/lib/schemas/session-schema.ts
+++ libs/auth/core/src/lib/schemas/session-schema.ts
@@ -4,18 +4,28 @@ import { EmailSchema } from './email-schema';
 import { FirstnameSchema, LastnameSchema } from './name-schema';
 import { UsernameSchema } from './username-schema';
 
+const AnonSessionSchema = z.object({
+  isAnon: z.literal(true),
+});
+
+export type AnonSession = z.infer<typeof AnonSessionSchema>;
+
+const LoggedInSessionSchema = z.object({
+  id: z.string().uuid(),
+  avatar: z.string(),
+  email: EmailSchema,
+  username: UsernameSchema,
+  firstname: FirstnameSchema,
+  lastname: LastnameSchema,
+  bio: z.optional(z.string()),
+  isAnon: z.literal(false),
+});
+
+export type LoggedInSession = z.infer<typeof LoggedInSessionSchema>;
+
 export const SessionSchema = z.discriminatedUnion('isAnon', [
-  z.object({ isAnon: z.literal(true) }),
-  z.object({
-    id: z.string().uuid(),
-    avatar: z.string(),
-    email: EmailSchema,
-    username: UsernameSchema,
-    firstname: FirstnameSchema,
-    lastname: LastnameSchema,
-    bio: z.optional(z.string()),
-    isAnon: z.literal(false),
-  }),
+  AnonSessionSchema,
+  LoggedInSessionSchema,
 ]);
 
 export type Session = z.infer<typeof SessionSchema>;
