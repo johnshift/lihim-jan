166714d822dbaa0c35a19e55b84cb30f43f10213
John Ballesteros <johnshift.dev@gmail.com>
2023-01-14 19:27:48 +0800

    refactor(auth-api): add get-email rpc

--- libs/auth/api/src/lib/constants.ts
+++ libs/auth/api/src/lib/constants.ts
@@ -1,2 +1,6 @@
+// String literals
 export const COOKEY_SESSION = 'LIHIM-SESSION';
 export const COOKEY_CSRF = 'LIHIM-CSRF';
+
+// Rpc names
+export const RPC_GET_EMAIL = 'get_email';
---
+++ libs/auth/api/src/lib/rpcs/get-email.ts
@@ -0,0 +1,53 @@
+import { EmailSchema, ERR_LOGIN_INCORRECT } from '@lihim/auth/core';
+import { createSupabaseClient } from '@lihim/shared/api';
+import { ApiError, ERR_INTERNAL } from '@lihim/shared/core';
+
+import { RPC_GET_EMAIL } from '../constants';
+
+// Supabase RPC generics
+type RpcName = typeof RPC_GET_EMAIL;
+type RpcSignature = {
+  Args: { principal: string };
+  Returns: string;
+};
+
+export const getEmail = async (_principal: string): Promise<string> => {
+  // Convert principal to lowercase
+  const principal = _principal.toLowerCase();
+
+  // If principal is email, no need to get-email
+  const parseResult = EmailSchema.safeParse(principal);
+  if (parseResult.success) {
+    return principal;
+  }
+
+  // Supabase anon client
+  const supabase = createSupabaseClient();
+
+  // Execute get-email rpc
+  const { data, error } = await supabase
+    .rpc<RpcName, RpcSignature>(RPC_GET_EMAIL, {
+      principal,
+    })
+    .single();
+
+  // This is expected to work, if error meaning on supabase's end -> return 500
+  if (error) {
+    console.error(
+      'getEmail postgres error:',
+      `\n\tcode=${error.code}`,
+      `\n\tmsg=${error.message}`,
+      `\n\thint=${error.hint}`,
+      `\n\tdetails=${error.details}`,
+    );
+    throw new Error(ERR_INTERNAL);
+  }
+
+  // If no email matches principal, incorrect signin
+  if (!data) {
+    throw new ApiError(400, ERR_LOGIN_INCORRECT);
+  }
+
+  // Data returns an array, we select only first element
+  return data.toLowerCase();
+};
