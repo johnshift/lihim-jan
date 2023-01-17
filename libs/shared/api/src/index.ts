c4e321a26d475183afb24256433a6973df0eca4d
John Ballesteros <johnshift.dev@gmail.com>
2023-01-17 20:35:49 +0800

    refactor(shared-api): removed previous supabase client

--- libs/shared/api/src/index.ts
+++ libs/shared/api/src/index.ts
@@ -1,4 +1,3 @@
 export { apiMiddleware } from './lib/api-middleware';
-export { createSupabaseClient } from './lib/create-supabase-client';
 export { supabaseRpc } from './lib/supabase-rpc';
 export type { MockApiRequest, MockApiResponse } from './lib/types';
