c262a0453ac2e56083746057eae6a6204a93c5a0
John Ballesteros <johnshift.dev@gmail.com>
2023-01-17 09:48:16 +0800

    chore: init playwright e2e

---
+++ apps/web-e2e/src/app.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '@playwright/test';
+
+test('should start page', async ({ page }) => {
+  await page.goto('/');
+
+  expect(true).toBeTruthy();
+  await page.pause();
+});
