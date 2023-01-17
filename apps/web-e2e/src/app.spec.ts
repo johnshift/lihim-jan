a95270100e582716b08dcd4557fd96afbacfa181
John Ballesteros <johnshift.dev@gmail.com>
2023-01-17 23:32:38 +0800

    chore: removed playwright nx-plugin

--- apps/web-e2e/src/app.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('should start page', async ({ page }) => {
-  await page.goto('/');
-
-  expect(true).toBeTruthy();
-  await page.pause();
-});
