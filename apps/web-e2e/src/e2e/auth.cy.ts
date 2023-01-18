5012d276f975b70969973406db75bd9a8c375ae8
John Ballesteros <johnshift.dev@gmail.com>
2023-01-18 20:38:38 +0800

    test: add auth end-to-end tests

--- apps/web-e2e/src/e2e/auth.cy.ts
+++ apps/web-e2e/src/e2e/auth.cy.ts
@@ -1,6 +1,25 @@
-import { ARIA_HEADER_MENU } from '@lihim/shared/core';
+import { ERR_EMAIL_TAKEN, ERR_USERNAME_TAKEN } from '@lihim/auth/core';
+import {
+  ARIA_SUBMIT_LOGIN,
+  ARIA_SUBMIT_SIGNUP,
+  ERR_SIGNUP_FAILED,
+  MSG_LOGIN_OK,
+  MSG_LOGIN_OK_INFO,
+  MSG_LOGOUT_DONE,
+  MSG_LOGOUT_OK,
+  MSG_SIGNUP_OK,
+  NAME_EMAIL,
+  NAME_FIRSTNAME,
+  NAME_LASTNAME,
+  NAME_PASSWORD,
+  NAME_PRINCIPAL,
+  NAME_USERNAME,
+  TESTID_SIGNUP_LINK,
+} from '@lihim/auth/core';
+import { fakeSignupPayload } from '@lihim/auth/testutils';
+import { ARIA_HEADER_MENU, TESTID_AUTH_MENUITEM } from '@lihim/shared/core';
 
-import { click } from '../support/helpers';
+import { click, input, select } from '../support/helpers';
 
 describe('auth', () => {
   beforeEach(() => {
@@ -8,7 +27,7 @@ describe('auth', () => {
   });
 
   // Since tests for auth depend on previous step,
-  // we want cypress to failfast if any steps failed
+  // we want cypress to failfast if any step failed
   afterEach(function () {
     if (this.currentTest.state === 'failed') {
       // @ts-expect-error Property runnner is not exposed by Cypress
@@ -16,9 +35,157 @@ describe('auth', () => {
     }
   });
 
-  it('should register user', () => {
+  const payload = fakeSignupPayload();
+
+  it('should signup user', () => {
+    // Open menu
     click('aria-label', ARIA_HEADER_MENU);
 
-    cy.pause();
+    // Open auth modal
+    click('data-testid', TESTID_AUTH_MENUITEM);
+
+    // Open signup form
+    click('data-testid', TESTID_SIGNUP_LINK);
+
+    // Fill in details
+    input(NAME_FIRSTNAME, payload.firstname);
+    input(NAME_LASTNAME, payload.lastname);
+    input(NAME_USERNAME, payload.username);
+    input(NAME_EMAIL, payload.email);
+    input(NAME_PASSWORD, payload.password);
+
+    // Submit
+    click('aria-label', ARIA_SUBMIT_SIGNUP);
+
+    // Wait for loading to disappear
+    select('data-loading', '').should('not.exist');
+
+    // Assert signup success indicators
+    cy.contains(MSG_SIGNUP_OK).should('be.visible');
+    cy.contains(`Welcome ${payload.firstname}!`).should('be.visible');
+
+    // Logout
+    click('aria-label', ARIA_HEADER_MENU);
+    click('data-testid', TESTID_AUTH_MENUITEM);
+
+    // Assert logout success indicators
+    cy.contains(MSG_LOGOUT_OK).should('be.visible');
+    cy.contains(MSG_LOGOUT_DONE).should('be.visible');
+  });
+
+  it('should login using email then logout', () => {
+    // Open menu
+    click('aria-label', ARIA_HEADER_MENU);
+
+    // Open auth modal
+    click('data-testid', TESTID_AUTH_MENUITEM);
+
+    // Fill in details (uppercased email to ensure case-insensitivity)
+    input(NAME_PRINCIPAL, payload.email.toUpperCase());
+    input(NAME_PASSWORD, payload.password);
+
+    // Submit
+    click('aria-label', ARIA_SUBMIT_LOGIN);
+
+    // Wait for loading to disappear
+    select('data-loading', '').should('not.exist');
+
+    // Assert success indicators
+    cy.contains(MSG_LOGIN_OK).should('be.visible');
+    cy.contains(MSG_LOGIN_OK_INFO).should('be.visible');
+
+    // Logout
+    click('aria-label', ARIA_HEADER_MENU);
+    click('data-testid', TESTID_AUTH_MENUITEM);
+
+    // Assert logout success indicators
+    cy.contains(MSG_LOGOUT_OK).should('be.visible');
+    cy.contains(MSG_LOGOUT_DONE).should('be.visible');
+  });
+
+  it('should login using username then logout', () => {
+    // Open menu
+    click('aria-label', ARIA_HEADER_MENU);
+
+    // Open auth modal
+    click('data-testid', TESTID_AUTH_MENUITEM);
+
+    // Fill in details (uppercased email to ensure case-insensitivity)
+    input(NAME_PRINCIPAL, payload.username.toUpperCase());
+    input(NAME_PASSWORD, payload.password);
+
+    // Submit
+    click('aria-label', ARIA_SUBMIT_LOGIN);
+
+    // Wait for loading to disappear
+    select('data-loading', '').should('not.exist');
+
+    // Assert success indicators
+    cy.contains(MSG_LOGIN_OK).should('be.visible');
+    cy.contains(MSG_LOGIN_OK_INFO).should('be.visible');
+
+    // Logout
+    click('aria-label', ARIA_HEADER_MENU);
+    click('data-testid', TESTID_AUTH_MENUITEM);
+
+    // Assert logout success indicators
+    cy.contains(MSG_LOGOUT_OK).should('be.visible');
+    cy.contains(MSG_LOGOUT_DONE).should('be.visible');
+  });
+
+  it('username taken error', () => {
+    // Open menu
+    click('aria-label', ARIA_HEADER_MENU);
+
+    // Open auth modal
+    click('data-testid', TESTID_AUTH_MENUITEM);
+
+    // Open signup form
+    click('data-testid', TESTID_SIGNUP_LINK);
+
+    // Fill in details
+    input(NAME_FIRSTNAME, payload.firstname);
+    input(NAME_LASTNAME, payload.lastname);
+    input(NAME_USERNAME, payload.username);
+    input(NAME_EMAIL, payload.email);
+    input(NAME_PASSWORD, payload.password);
+
+    // Submit
+    click('aria-label', ARIA_SUBMIT_SIGNUP);
+
+    // Wait for loading to disappear
+    select('data-loading', '').should('not.exist');
+
+    // Assert success indicators
+    cy.contains(ERR_SIGNUP_FAILED).should('be.visible');
+    cy.contains(ERR_USERNAME_TAKEN).should('be.visible');
+  });
+
+  it('email taken error', () => {
+    // Open menu
+    click('aria-label', ARIA_HEADER_MENU);
+
+    // Open auth modal
+    click('data-testid', TESTID_AUTH_MENUITEM);
+
+    // Open signup form
+    click('data-testid', TESTID_SIGNUP_LINK);
+
+    // Fill in details
+    input(NAME_FIRSTNAME, payload.firstname);
+    input(NAME_LASTNAME, payload.lastname);
+    input(NAME_USERNAME, payload.username + 'x');
+    input(NAME_EMAIL, payload.email);
+    input(NAME_PASSWORD, payload.password);
+
+    // Submit
+    click('aria-label', ARIA_SUBMIT_SIGNUP);
+
+    // Wait for loading to disappear
+    select('data-loading', '').should('not.exist');
+
+    // Assert success indicators
+    cy.contains(ERR_SIGNUP_FAILED).should('be.visible');
+    cy.contains(ERR_EMAIL_TAKEN).should('be.visible');
   });
 });
