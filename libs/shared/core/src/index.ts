52c3dbfefb939afb45fa960d04cdd20f5b708d0b
John Ballesteros <johnshift.dev@gmail.com>
2023-01-20 10:30:44 +0800

    refactor(shared-core): add num formatter

--- libs/shared/core/src/index.ts
+++ libs/shared/core/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/constants';
 export * from './lib/errors';
+export * from './lib/misc';
 export * from './lib/schemas';
 export * from './lib/types';
 export * from './lib/utils';
